358de47c49830cdb06ba3eac6385c61ebfdc6b5f
akash-plugseal <akash.kumar@plugseal.in>
2026-01-30 15:05:00 +0530

    feat: add sum module bug: check number validation tech: validation package, radis, express

--- src/app.js
+++ src/app.js
@@ -1,1 +1,12 @@
 console.log("app")
+
+
+const sum = (num1, num2) =>{
+    if(typeof num1 !== 'number' || typeof num2 !== 'number'){
+        console.log("must be a number")
+        return;
+    }
+    return num1 + num2;
+}
+
+sum(12, 32);
